a3e0f66c656ca14e5a85217484fbc0d43ba1e60e
josue.cubero@ucr.ac.cr <jo96cube@gmail.com>
2018-08-28 10:43:41 -0600

    Header styles ready

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,8 +6,7 @@
 import React, { Component } from 'react';
 
 // Reactstrap
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle,
-    DropdownMenu, DropdownItem } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 // Font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -45,20 +44,45 @@ class Header extends Component {
     render() {
         return (
             <header className='header'>
-                <Navbar color="light" light expand="sm">
-                    <NavbarBrand href="/" className='w-25'>
-                        <img src={logo} className='w-50'/>
+                <Navbar expand="md" light>
+                    <NavbarBrand href="/" className='header__brand'>
+                        <img src={logo} className='w-100'/>
                     </NavbarBrand>
                     <NavbarToggler onClick={this.toggle}/>
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
-                            <NavItem className='header__item mr-5'>
-                                <Link to={'/about'} className='header__link'>
-                                    About
-                                </Link>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar className='mr-5 pr-3'>
-                                <DropdownToggle nav caret>
+                            <Link to='/createEvent' className='header__link'>
+                                <NavItem className='py-2 pl-3 mr-1'>
+                                    <FontAwesomeIcon icon="calendar" className='mr-2'/> Create
+                                </NavItem>
+                            </Link>
+                            <Link to='/myEvents' className='header__link'>
+                                <NavItem className='py-2 pl-3 mr-1'>
+                                    <FontAwesomeIcon icon="calendar-alt" className='mr-2'/> My events
+                                </NavItem>
+                            </Link>
+                            <Link to='/enrolledEvents' className='header__link'>
+                                <NavItem className='py-2 pl-3 mr-1'>
+                                    <FontAwesomeIcon icon="calendar-check" className='mr-2'/> Enrolled
+                                </NavItem>
+                            </Link>
+                            <Link to='/topEvents' className='header__link'>
+                                <NavItem className='py-2 pl-3 mr-1'>
+                                    <FontAwesomeIcon icon="calendar-plus" className='mr-2'/> Top rated
+                                </NavItem>
+                            </Link>
+                            <Link to='/' className='header__link header__user-dropdown-item' onClick={this.logout}>
+                                <NavItem className='py-2 pl-3 mr-1'>
+                                    <FontAwesomeIcon icon="sign-out-alt" className='mr-2'/> Logout
+                                </NavItem>
+                            </Link>
+                            <Link to='/profile' className='header__link'>
+                                <NavItem className='py-2 pl-3 mr-1 header__user-dropdown-item'>
+                                    <FontAwesomeIcon icon="user-cog" className='mr-2'/> Profile
+                                </NavItem>
+                            </Link>
+                            <UncontrolledDropdown nav inNavbar className='mr-5 pr-3 header__user-dropdown'>
+                                <DropdownToggle nav caret className='pl-3'>
                                     <FontAwesomeIcon icon="user" />
                                 </DropdownToggle>
                                 <DropdownMenu right>
